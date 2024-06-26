a52fa9fa0225623ab4aa5c21b353f083bfa3d5bb
ahmedguemues <ahmed.guemues47@gmail.comm>
2024-06-26 14:05:09 +0200

    Initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BelegverfolgungTool;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
